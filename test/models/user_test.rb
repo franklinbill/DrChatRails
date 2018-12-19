3c445ffbf582fffd11a917d5780c83db07a0bdfa
liuzhe <994771138@qq.com>
2018-12-19 16:58:23 +0800

    delete files of fixture/ add test/models/user_test.rb name & email & password test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,14 +5,25 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "User1", email: "user1@test.com")
+    @user = User.new(name: "User1", email: "user1@test.com", password:"password")
   end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
   test "name should be present" do
     @user.name = " "
     assert_not @user.valid?
   end
+
   test "email should be present" do
     @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = " "
     assert_not @user.valid?
   end
 end
